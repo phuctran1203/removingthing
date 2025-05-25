7a66bfbc2992a06cf98c6271e5da79b4efc96fc5
Phuc Tran <phuctran12032004@gmail.com>
2025-05-25 21:51:02 +0700

    change homepage

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Home from "./Home";
 
 function App() {
   return (
-    <BrowserRouter basename="/linkbridge">
+    <BrowserRouter basename="/LinkBridge">
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
